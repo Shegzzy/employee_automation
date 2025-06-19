08ab1e2204c85e3521a4a4d6087e622bc878eaeb
MichaelAppentus <michhubs@gmail.com>
2025-06-19 16:34:09 +0100

    added api services

---
+++ lib/service/api_services/api_services.dart
@@ -0,0 +1,16 @@
+import '../../common/io/data.dart';
+import '../../models/employee_model.dart';
+
+class ApiService {
+  Future<List<Employee>> successResponse() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<dynamic> rawData = Api.successResponse['data'] ?? [];
+    return rawData.map((e) => Employee.fromJson(e)).toList();
+  }
+
+  Future<List<Employee>> errorResponse() async {
+    await Future.delayed(const Duration(seconds: 1));
+    throw Exception(Api.errorResponse['message'] ?? 'Unknown error');
+  }
+}
