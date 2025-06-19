9c6624d23e732d9807b0bda5bb0c1288bb30ec72
MichaelAppentus <michhubs@gmail.com>
2025-06-19 11:54:03 +0100

    project start up

---
+++ lib/modules/widgets/employee_card/employee_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ShadowContainer extends StatelessWidget {
+  final Widget child;
+  const ShadowContainer({
+    super.key, required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+          top: Dimensions.height8),
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 0,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // Adjust the offset
+            ),
+          ],
+          borderRadius: BorderRadius.circular(
+              Dimensions.radius8 - 4),
+          color: Colors.white
+      ),
+      child: child,
+    );
+  }
+}
